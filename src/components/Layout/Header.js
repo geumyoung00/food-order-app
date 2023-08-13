ccec82ac4644f0adf448f58f2bb242b6832a0946
geumyoung <1gyn093@gmail.com>
2023-08-14 00:27:45 +0900

    Modified styles to classes and CartIcon component to self-closing

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,16 @@
 import React from "react";
-import styles from "./Header.module.css";
+import classes from "./Header.module.css";
 import imgHeader from "./meals.jpg";
 import HeaderCartButton from "./HeaderCartButton/HeaderCartButton";
 
 const Header = () => {
   return (
     <>
-      <div className={`${styles["header"]}`}>
+      <div className={classes.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton />
       </div>
-      <div className={`${styles["main-image"]}`}>
+      <div className={classes["main-image"]}>
         <img src={imgHeader} alt="Header background image" />
       </div>
     </>
--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,15 +1,15 @@
 import React from "react";
-import styles from "./HeaderCartButton.module.css";
+import classes from "./HeaderCartButton.module.css";
 import CartIcon from "./CartIcon";
 
 const HeaderCartButton = () => {
   return (
-    <button className={`${styles["button"]} ${styles["bump"]}`}>
-      <div className={`${styles["icon"]}`}>
-        <CartIcon></CartIcon>
+    <button className={`${classes.button} ${classes.bump}`}>
+      <div className={classes.icon}>
+        <CartIcon />
       </div>
       Your Cart
-      <div className={`${styles["badge"]}`}>0</div>
+      <div className={classes.badge}>0</div>
     </button>
   );
 };
